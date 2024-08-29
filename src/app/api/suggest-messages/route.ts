844495f5be08a0a26ed6dd626407f5e88e144c28
Priyanshu Sinha <priyanshusinha.2807@gmail.com>
2024-08-29 23:49:56 +0530

    implemented gen ai to generate messages

---
+++ src/app/api/suggest-messages/route.ts
@@ -0,0 +1,32 @@
+import { NextResponse } from "next/server";
+import { GoogleGenerativeAI } from "@google/generative-ai";
+
+
+const genAI = new GoogleGenerativeAI(process.env.GOOGLE_GENERATIVE_AI_API_KEY!);
+const model = genAI.getGenerativeModel({ model: "gemini-pro" });
+
+export async function POST(req: Request) {
+    const prompt =
+        "Create a list of three open-ended and engaging questions formatted as a single string. Each question should be separated by '||'. These questions are for an anonymous social messaging platform, like Qooh.me, and should be suitable for a diverse audience. Avoid personal or sensitive topics, focusing instead on universal themes that encourage friendly interaction. For example, your output should be structured like this: 'What’s a hobby you’ve recently started?||If you could have dinner with any historical figure, who would it be?||What’s a simple thing that makes you happy?'. Ensure the questions are intriguing, foster curiosity, and contribute to a positive and welcoming conversational environment.";
+
+    try {
+       
+        const response = await model.generateContent(prompt);
+
+        return NextResponse.json(
+            {
+                success: true,
+                message: response.response
+            },
+            { status: 200 }
+        );
+    } catch (error: any) {
+        return NextResponse.json(
+            {
+                success: true,
+                message: error,
+            },
+            { status: 500 }
+        );
+    }
+}
