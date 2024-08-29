741ab747974d0823e5bcb343d4910f62a1e01e0d
Priyanshu Sinha <priyanshusinha.2807@gmail.com>
2024-08-29 17:14:18 +0530

    verification email bugs

--- src/helpers/sendVerificationEmail.ts
+++ src/helpers/sendVerificationEmail.ts
@@ -9,7 +9,7 @@ export async function sendVerficationEmail(
 ): Promise<ApiResponse> {
     try {
         await resend.emails.send({
-            from: 'you@example.com',
+            from: 'Acme <onboarding@resend.dev>',
             to: email,
             subject: 'AMA app | Verifcation Code',
             react: VerificationEmail({ username, otp: verifyCode }),
