7334ae7654d1941c3344e861a813dc5d87489167
Priyanshu Sinha <priyanshusinha.2807@gmail.com>
2024-08-30 04:00:42 +0530

    gen ai generation

--- src/app/api/suggest-messages/route.ts
+++ src/app/api/suggest-messages/route.ts
@@ -1,7 +1,11 @@
+import OpenAI from "openai";
 import { NextResponse } from "next/server";
 import { GoogleGenerativeAI } from "@google/generative-ai";
 
-
+// const genAi = createGoogleGenerativeAI({
+//   baseURL: "https://generativelanguage.googleapis.com/v1beta",
+//   apiKey: process.env.GOOGLE_GENERATIVE_AI_API_KEY,
+// });
 const genAI = new GoogleGenerativeAI(process.env.GOOGLE_GENERATIVE_AI_API_KEY!);
 const model = genAI.getGenerativeModel({ model: "gemini-pro" });
 
@@ -10,7 +14,7 @@ export async function POST(req: Request) {
         "Create a list of three open-ended and engaging questions formatted as a single string. Each question should be separated by '||'. These questions are for an anonymous social messaging platform, like Qooh.me, and should be suitable for a diverse audience. Avoid personal or sensitive topics, focusing instead on universal themes that encourage friendly interaction. For example, your output should be structured like this: 'What’s a hobby you’ve recently started?||If you could have dinner with any historical figure, who would it be?||What’s a simple thing that makes you happy?'. Ensure the questions are intriguing, foster curiosity, and contribute to a positive and welcoming conversational environment.";
 
     try {
-       
+        // Ask OpenAI for a streaming chat completion given the prompt
         const response = await model.generateContent(prompt);
 
         return NextResponse.json(
--- src/app/u/[username]/page.tsx
+++ src/app/u/[username]/page.tsx
@@ -1,14 +1,5 @@
-'use client';
-
-import React, { useState } from 'react';
-import axios, { AxiosError } from 'axios';
-import { zodResolver } from '@hookform/resolvers/zod';
-import { useForm } from 'react-hook-form';
-import { Loader2 } from 'lucide-react';
-import { Button } from '@/components/ui/button';
-import { Separator } from '@/components/ui/separator';
-import { CardHeader, CardContent, Card } from '@/components/ui/card';
-import { useCompletion } from 'ai/react';
+"use client";
+import React, { useState } from "react";
 import {
   Form,
   FormControl,
@@ -16,164 +7,177 @@ import {
   FormItem,
   FormLabel,
   FormMessage,
-} from '@/components/ui/form';
-import { Textarea } from '@/components/ui/textarea';
-import { toast } from '@/components/ui/use-toast';
-import * as z from 'zod';
-import { ApiResponse } from '@/types/ApiResponse';
-import Link from 'next/link';
-import { useParams } from 'next/navigation';
-import { messageSchema } from '@/schemas/messageSchema';
+} from "@/components/ui/form";
+import { useForm } from "react-hook-form";
+import { Textarea } from "@/components/ui/textarea";
+import { Button } from "@/components/ui/button";
+import { useParams } from "next/navigation";
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import { Separator } from "@/components/ui/separator";
+import Link from "next/link";
+import { messageSchema } from "@/schemas/messageSchema";
+import * as z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import axios, { Axios } from "axios";
+import { useToast } from "@/components/ui/use-toast";
+import { Loader2 } from "lucide-react";
 
-const specialChar = '||';
-
-const parseStringMessages = (messageString: string): string[] => {
-  return messageString.split(specialChar);
-};
-
-const initialMessageString =
-  "What's your favorite movie?||Do you have any pets?||What's your dream job?";
-
-export default function SendMessage() {
-  const params = useParams<{ username: string }>();
-  const username = params.username;
-
-  const {
-    complete,
-    completion,
-    isLoading: isSuggestLoading,
-    error,
-  } = useCompletion({
-    api: '/api/suggest-messages',
-    initialCompletion: initialMessageString,
-  });
-
+function Page() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [isSuggestButtonLoading, setIsSuggestButtonLoading] = useState(false);
+  const { toast } = useToast();
+  const [text, setText] = useState("");
   const form = useForm<z.infer<typeof messageSchema>>({
     resolver: zodResolver(messageSchema),
+    defaultValues: {
+      content: "",
+    },
   });
+  const watchContent = form.watch("content");
 
-  const messageContent = form.watch('content');
+  const initialMessageString =
+    "What's your favorite movie?||Do you have any pets?||What's your dream job?";
+  const params = useParams<{ username: string }>();
+  const specialChar = "||";
 
-  const handleMessageClick = (message: string) => {
-    form.setValue('content', message);
+  const StringSplit = (sentence: string): string[] => {
+    return sentence.split(specialChar);
   };
 
-  const [isLoading, setIsLoading] = useState(false);
-
-  const onSubmit = async (data: z.infer<typeof messageSchema>) => {
+  async function onMessageSubmmit(data: z.infer<typeof messageSchema>) {
     setIsLoading(true);
-    try {
-      const response = await axios.post<ApiResponse>('/api/send-message', {
-        ...data,
-        username,
-      });
 
-      toast({
-        title: response.data.message,
-        variant: 'default',
+    try {
+      const response = await axios.post("/api/send-message", {
+        username: params.username,
+        content: data.content,
       });
-      form.reset({ ...form.getValues(), content: '' });
-    } catch (error) {
-      const axiosError = error as AxiosError<ApiResponse>;
+      if (response.data.success) {
+        toast({
+          title: "success",
+          description: response.data.message,
+        });
+      }
+
+      form.setValue("content", "");
+    } catch (error: any) {
+      console.log(error);
       toast({
-        title: 'Error',
-        description:
-          axiosError.response?.data.message ?? 'Failed to sent message',
-        variant: 'destructive',
+        title: "error",
+        description: error.response.data.message,
+        variant: "default",
       });
     } finally {
       setIsLoading(false);
     }
-  };
+  }
 
-  const fetchSuggestedMessages = async () => {
+  function handleTextMessage(data: string) {
+    form.setValue("content", data);
+  }
+  async function onSuggestMessage() {
+    setIsSuggestButtonLoading(true);
     try {
-      complete('');
-    } catch (error) {
-      console.error('Error fetching messages:', error);
-      // Handle error appropriately
+      const result = await axios.post("/api/suggest-messages");
+      const response = result.data.message.candidates[0].content.parts[0].text;
+      setText(response);
+      return response;
+    } catch (error: any) {
+      toast({
+        title: "error",
+        description: error.message,
+        variant: "destructive",
+      });
+    } finally {
+      setIsSuggestButtonLoading(false);
     }
-  };
-
+  }
   return (
-    <div className="container mx-auto my-8 p-6 bg-white rounded max-w-4xl">
+    <div className="container mx-auto my-8 p-6  rounded max-w-4xl">
       <h1 className="text-4xl font-bold mb-6 text-center">
         Public Profile Link
       </h1>
       <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+        <form
+          className=" space-y-6"
+          onSubmit={form.handleSubmit(onMessageSubmmit)}
+        >
           <FormField
             control={form.control}
             name="content"
             render={({ field }) => (
-              <FormItem>
-                <FormLabel>Send Anonymous Message to @{username}</FormLabel>
+              <>
+                <FormLabel>
+                  {" "}
+                  Send Anonymous Message to @{params.username}
+                </FormLabel>
                 <FormControl>
                   <Textarea
-                    placeholder="Write your anonymous message here"
-                    className="resize-none"
+                    placeholder="Write your anonymous message here "
+                    className="resize-none "
                     {...field}
                   />
                 </FormControl>
-                <FormMessage />
-              </FormItem>
+              </>
             )}
-          />
-          <div className="flex justify-center">
-            {isLoading ? (
-              <Button disabled>
-                <Loader2 className="mr-2 h-4 w-4 animate-spin" />
-                Please wait
-              </Button>
-            ) : (
-              <Button type="submit" disabled={isLoading || !messageContent}>
-                Send It
-              </Button>
-            )}
-          </div>
+          ></FormField>
+          {isLoading ? (
+            <Button disabled>
+              <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+              Please wait
+            </Button>
+          ) : (
+            <Button type="submit" disabled={isLoading || !watchContent}>
+              Send It
+            </Button>
+          )}
         </form>
       </Form>
-
       <div className="space-y-4 my-8">
         <div className="space-y-2">
           <Button
-            onClick={fetchSuggestedMessages}
             className="my-4"
-            disabled={isSuggestLoading}
+            onClick={onSuggestMessage}
+            disabled={isSuggestButtonLoading}
           >
-            Suggest Messages
+            Suggest Message{" "}
           </Button>
           <p>Click on any message below to select it.</p>
         </div>
         <Card>
-          <CardHeader>
-            <h3 className="text-xl font-semibold">Messages</h3>
-          </CardHeader>
-          <CardContent className="flex flex-col space-y-4">
-            {error ? (
-              <p className="text-red-500">{error.message}</p>
-            ) : (
-              parseStringMessages(completion).map((message, index) => (
+          <CardHeader className="font-bold "> Messages</CardHeader>
+          <CardContent className="flex flex-col space-y-4 ">
+            {text == ""
+              ? StringSplit(initialMessageString).map((data, index) => (
                 <Button
+                  className="border"
                   key={index}
-                  variant="outline"
-                  className="mb-2"
-                  onClick={() => handleMessageClick(message)}
+                  onClick={() => handleTextMessage(data)}
                 >
-                  {message}
+                  {data}
                 </Button>
               ))
-            )}
+              : StringSplit(text).map((data, index) => (
+                <Button
+                  className="bg-transparent border  "
+                  key={index}
+                  onClick={() => handleTextMessage(data)}
+                >
+                  {data}
+                </Button>
+              ))}
           </CardContent>
         </Card>
       </div>
       <Separator className="my-6" />
       <div className="text-center">
         <div className="mb-4">Get Your Message Board</div>
-        <Link href={'/sign-up'}>
+        <Link href={"/sign-up"}>
           <Button>Create Your Account</Button>
         </Link>
       </div>
     </div>
   );
 }
+
+export default Page;
